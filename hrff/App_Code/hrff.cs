23886407f2f04f953cff313220f12319a78f927e
Jiykoth <nathan.weisser@softvu.com>
2017-10-12 16:49:58 -0500

    Common class added

--- hrff/App_Code/hrff.cs
+++ hrff/App_Code/hrff.cs
@@ -1,1 +1,45 @@
-﻿
+﻿using System;
+using System.Data;
+using System.Web.Script.Serialization;
+using System.Collections.Generic;
+using System.Web.Configuration;
+using System.Data.SqlClient;
+
+namespace Hrff { 
+  public class Common {
+    
+    public static string GetTable(string sql) {
+      using (SqlConnection conn = Common.GetConn()) {
+        using (SqlCommand cmd = new SqlCommand(sql, conn)) {
+          using (SqlDataAdapter da = new SqlDataAdapter(cmd)) {
+            using (DataSet ds = new DataSet()) {
+              da.Fill(ds);
+              return Common.GetDataTableToJSONString(ds.Tables[0]);
+            }
+          }
+        }
+      }
+    }
+
+    private static SqlConnection GetConn() {
+      string connString = WebConfigurationManager.ConnectionStrings["localConnectionString"].ConnectionString;
+      SqlConnection conn = new SqlConnection(connString);
+      conn.Open();
+      return conn;
+    }
+
+    private static string GetDataTableToJSONString(DataTable table) {
+      List<Dictionary<string, object>> list = new List<Dictionary<string, object>>();
+      foreach (DataRow row in table.Rows) {
+        Dictionary<string, object> dict = new Dictionary<string, object>();
+        foreach (DataColumn col in table.Columns) {
+          dict[col.ColumnName] = row[col];
+        }
+        list.Add(dict);
+      }
+      JavaScriptSerializer serializer = new JavaScriptSerializer();
+      serializer.MaxJsonLength = Int32.MaxValue;
+      return serializer.Serialize(list);
+    }
+  }
+}
--- hrff/default.aspx
+++ hrff/default.aspx
@@ -1,10 +1,15 @@
-﻿<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <title></title>
-</head>
-<body>
-      Heidi Ho, Neighbor! Ooga Booga!
-</body>
-</html>
+﻿<%@ Import Namespace="System" %>
+<%@ Import Namespace="System.Web.Configuration" %>
+<%@ Import Namespace="Hrff" %>
+<%@ Page Language="C#"%>
+
+<script language="cs" runat="server">
+
+  private string result = "";
+  protected void Page_Load(object sender, EventArgs e) {
+    result = Common.GetTable("select * from dbo.competencies;");
+  }
+
+</script>
+
+<%=result%>
